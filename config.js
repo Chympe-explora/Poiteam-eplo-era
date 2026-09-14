45529a20bc061372a81672ef5d751da6f0c420c2
Chympe-explora <senlysuchiang87@gmail.com>
2026-09-14 19:00:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -69,7 +69,7 @@
   "whatsappNumber": "916001877518",
   "logoImage": "logo.png",
   "backgroundvideo": "hero-video.mp4",
-  "instagram": "https://www.instagram.com/team_explo_era?stkn=MWxwcXIzZDh1ajJoMw==",
+  "instagram": "https://www.instagram.com/team_explo_era?stkn=ZHZpODB3aXl0bXBu",
 
   // ==========================================================
   // 🎬 SITE-WIDE BACKGROUND VIDEO
