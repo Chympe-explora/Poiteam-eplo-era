4c7291fb36666c53171bff9b8bca270415d3cc6a
Chympe-explora <senlysuchiang87@gmail.com>
2026-09-14 19:01:56 +0530

    Update Instagram link in config.js

--- krem-chympe/config.js
+++ krem-chympe/config.js
@@ -239,7 +239,7 @@
       },
 
       // ---- Contact & payment details ----
-      instagram: "https://www.instagram.com/team_explo_era?stkn=MWxwcXIzZDh1ajJoMw==",
+      instagram: "https://www.instagram.com/team_explo_era?stkn=ZHZpODB3aXl0bXBu",
       whatsappNumber: "916001877518",
       upiId: "8787679579@ybl",
       bank: {
